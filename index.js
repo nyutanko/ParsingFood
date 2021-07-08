b975bee86e1bea20ecff14554c896b51b95d8634
Nyuta B <anyuta.nyuta333@gmail.com>
2021-07-08 20:43:17 +0300

    #1. Working with input

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log('Hello')
+const csv = require('csv-parser')
+const fs = require('fs')
+const puppeteer = require('puppeteer')
+
+const results = []
+const link = 'https://www.doordash.com/'
+
+fs.createReadStream('Anya_Config.csv')
+    .pipe(csv())
+    .on('data', (data) => results.push(data))
+    .on('end', () => {
+    });
+
+(async () => {
+  try {
+    const browser = await puppeteer.launch({ headless: false })
+    const page = await browser.newPage()
+
+    await page.goto(`${link}`)
+    await page.waitForTimeout(4000)
+
+    const input = await page.waitForXPath("//input[@aria-label='Your delivery address']")
+    await input.type('New York, NY', { delay: 500 })
+
+    await page.click('[aria-label="Find Restaurants"]')
+    //await browser.close()
+  } catch (e) {
+    console.log(e)
+  }
+})()
